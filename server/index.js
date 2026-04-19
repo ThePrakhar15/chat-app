a936f422d4c61fb02ec5f833d6d15979d01f1ee1
ThePrakhar15 <prakharsharan15@gmail.com>
2026-04-19 15:31:28 +0530

    fix: env security + remove duplicate DB connection + deploy ready

--- server/index.js
+++ server/index.js
@@ -15,9 +15,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/chatapp")
-  .then(() => console.log(" MongoDB Connected"))
-  .catch((err) => console.log(err));
 
 app.use("/auth", authRoutes);
 
